98d8cb776179deba1d60a147f030eb0cb5a4ed61
Dikshita Patel <dikshipatel1999@gmail.com>
2024-02-27 19:05:53 -0500

    Initial Commit

---
+++ myMovieListApp/myMovieListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMovieListAppApp.swift
+//  myMovieListApp
+//
+//  Created by Dikshita Rajendra Patel on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct myMovieListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
